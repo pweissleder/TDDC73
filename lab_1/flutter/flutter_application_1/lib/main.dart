49d08d6941f86e5f7cd7115cbe0393016cdb2970
Pascal Weissleder <pascal.weissleder.bs@gmail.com>
2024-11-15 16:53:05 +0100

    Flutter implemented

--- lab_1/flutter/flutter_application_1/lib/main.dart
+++ lab_1/flutter/flutter_application_1/lib/main.dart
@@ -9,11 +9,117 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text("Example 1"),
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.teal,
+          centerTitle: false,
         ),
+        body: const ExampleUI(),
+      ),
+    );
+  }
+}
+
+class ExampleUI extends StatelessWidget {
+  const ExampleUI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            '/Users/pascal/Documents/Repositories/Uni/TDDC73/lab_1/flutter/flutter_application_1/assets/circle_image.png', // Adjust path if needed
+            width: 150,
+            height: 150,
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("BUTTON"),
+                style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: Color(0xFFD6D7D7),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 24, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(4), // Less rounded corners
+                    )),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("BUTTON"),
+                style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: Color(0xFFD6D7D7),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 24, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(4), // Less rounded corners
+                    )),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("BUTTON"),
+                style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: Color(0xFFD6D7D7),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 24, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(4), // Less rounded corners
+                    )),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("BUTTON"),
+                style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: Color(0xFFD6D7D7),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 24, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(4), // Less rounded corners
+                    )),
+              ),
+            ],
+          ),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "Email",
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.pink),
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.pink),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.pink),
+                ),
+                labelStyle: TextStyle(color: Colors.grey),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
